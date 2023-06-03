e310a55a1ba9fca49a0d4de78c9555f8de054635
faridefendi44 <103506992+faridefendi44@users.noreply.github.com>
2023-06-03 19:47:50 +0700

    fungsi login register done

--- app/src/main/java/com/example/wisma/Login.java
+++ app/src/main/java/com/example/wisma/Login.java
@@ -1,110 +1,102 @@
 package com.example.wisma;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.IntentSenderRequest;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class Login extends AppCompatActivity {
 
-    private SignInClient oneTapClient;
-    private BeginSignInRequest signUpRequest;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    EditText logusername, logpassword;
+    MaterialButton loginbtn;
+    TextView register;
 
-    ImageView login_google;
-    Button button;
-
-    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
-    private boolean showOneTapUI = true;
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
 
-        login_google=findViewById(R.id.login_google);
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        logusername = findViewById(R.id.logusername);
+        logpassword = findViewById(R.id.logpassword);
+        loginbtn = findViewById(R.id.loginbtn);
+        progressBar = findViewById(R.id.progressBar);
+        register = findViewById(R.id.signup);
 
-
-        oneTapClient = Identity.getSignInClient(this);
-        signUpRequest = BeginSignInRequest.builder()
-                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                        .setSupported(true)
-                        // Your server's client ID, not your Android client ID.
-                        .setServerClientId(getString(R.string.web_client_id))
-                        // Show all accounts on the device.
-                        .setFilterByAuthorizedAccounts(false)
-                        .build())
-                .build();
-
-        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
-                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK){
-
-                            try {
-                                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
-                                String idToken = credential.getGoogleIdToken();
-                                if (idToken !=  null) {
-                                    // Got an ID token from Google. Use it to authenticate
-                                    // with your backend.
-                                    String email = credential.getId();
-                                    Toast.makeText(getApplicationContext(), "Email"+email, Toast.LENGTH_SHORT).show();
-                                }
-                            } catch (ApiException e) {
-                                // ...
-                                e.printStackTrace();
-                            }
-
-                        }
-                    }
-                });
-
-
-
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                oneTapClient.beginSignIn(signUpRequest)
-                        .addOnSuccessListener(Login.this, new OnSuccessListener<BeginSignInResult>() {
-                            @Override
-                            public void onSuccess(BeginSignInResult result) {
-                                IntentSenderRequest intentSenderRequest =
-                                        new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
-                                activityResultLauncher.launch(intentSenderRequest);
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(logusername.getText());
+                password = String.valueOf(logpassword.getText());
 
-                            }
-                        })
-                        .addOnFailureListener(Login.this, new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // No Google Accounts found. Just continue presenting the signed-out UI.
-                                Log.d("TAG", e.getLocalizedMessage());
-                            }
-                        });
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this, "Enter Username", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Login.this, "Authentication succes.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(Login.this, Dashboard.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+
+                }
             }
         });
+
+
     }
 }
