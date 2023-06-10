c7d21a6f124bcaafed32cf31b00874f5d8b049ab
YOGA <purwonoyoga@gmail.com>
2023-06-10 22:20:39 +0700

    comit

---
+++ app/src/main/java/com/example/wisma/Welcome2.java
@@ -0,0 +1,25 @@
+package com.example.wisma;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Welcome2 extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    ImageView aboutus, logout;
+    FirebaseUser user;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_2);
+
+    }
+    }
