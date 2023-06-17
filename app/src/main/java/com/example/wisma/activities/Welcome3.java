29c4b4d33e6a6c1cdadb89d6891eb36491ea3caf
fferdinandpp <ferditetsuya22@gmail.com>
2023-06-18 01:11:02 +0700

    splash

--- app/src/main/java/com/example/wisma/activities/Welcome3.java
+++ app/src/main/java/com/example/wisma/activities/Welcome3.java
@@ -1,6 +1,8 @@
 package com.example.wisma.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,6 +16,11 @@ public class Welcome3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility (
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.welcome_3);
 
         ImageView next = findViewById(R.id.btn_next);
