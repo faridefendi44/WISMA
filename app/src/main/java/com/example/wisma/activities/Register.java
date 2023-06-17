1a7e8281a2fa24ace70c1b56d7d67d68fd22e6d6
faridefendi44 <faridnurefendi19@gmail.com>
2023-06-18 03:46:21 +0700

    comit

--- app/src/main/java/com/example/wisma/activities/Register.java
+++ app/src/main/java/com/example/wisma/activities/Register.java
@@ -1,21 +1,21 @@
 package com.example.wisma.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.wisma.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -25,7 +25,7 @@ public class Register extends AppCompatActivity {
     FirebaseAuth mAuth;
     ProgressBar progressBar;
     EditText regusername, name, regpassword;
-    MaterialButton regbtn;
+    Button regbtn;
     TextView login;
 
 //    @Override
