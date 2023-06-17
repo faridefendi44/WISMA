d3ac7ca1dca345a4c045459db249d7570a0d2ded
fferdinandpp <ferditetsuya22@gmail.com>
2023-06-17 20:37:05 +0700

    welcome

--- app/src/main/java/com/example/wisma/activities/Welcome1.java
+++ app/src/main/java/com/example/wisma/activities/Welcome1.java
@@ -1,26 +1,28 @@
 package com.example.wisma.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wisma.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class Welcome1 extends AppCompatActivity {
-
-    FirebaseAuth auth;
-    ImageView aboutus, logout;
-    FirebaseUser user;
-    FirebaseAuth mAuth;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_1);
 
+        ImageView next = findViewById(R.id.btn_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome1.this, Welcome2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wisma/activities/Welcome2.java
+++ app/src/main/java/com/example/wisma/activities/Welcome2.java
@@ -1,26 +1,28 @@
 package com.example.wisma.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wisma.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class Welcome2 extends AppCompatActivity {
-
-    FirebaseAuth auth;
-    ImageView aboutus, logout;
-    FirebaseUser user;
-    FirebaseAuth mAuth;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_2);
 
+        ImageView next = findViewById(R.id.btn_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome2.this, Welcome3.class);
+                startActivity(intent);
+            }
+        });
     }
-    }
+}
